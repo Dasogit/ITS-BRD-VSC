a0060b9d0d9175348995447fb26375754a1a492e
Dasogit <danandehmsohrab@gmail.com>
2025-10-30 17:54:50 +0100

    FOR REAL DONE!!!!!

--- Programs/Aufgabe/Src/errorHandler.c
+++ Programs/Aufgabe/Src/errorHandler.c
@@ -7,7 +7,6 @@
 
 #include "errorHandler.h"
 #include "display.h"
-#include <errno.h>
 #include <stdio.h>
 #include <limits.h>
 
@@ -17,43 +16,26 @@
  *  @param     error number
  *  @return    returns error number
  ****************************************************************************************/
-int handleError(int errorNumber) {
+void handleError(int errorNumber) {
+  setErrMode();
   switch (errorNumber) {
   case userStackOverFlow:
-    errno = userStackOverFlow;
-    setErrMode();
-    printf((char *)STACKOVERMSG);
-    return errno;
+    printf(STACKOVERMSG);
     break;
   case userStackUnderFlow:
-    errno = userStackUnderFlow;
-    setErrMode();
-    printf((char *)STACKUNDERMSG);
-    return errno;
+    printf(STACKUNDERMSG);
     break;
   case userArithmeticOverflow:
-    errno = userArithmeticOverflow;
-    setErrMode();
-    printf((char *)ARTOVERMSG);
-    return errno;
+    printf(ARTOVERMSG);
     break;
   case userArithemticUnderflow:
-    errno = userArithemticUnderflow;
-    setErrMode();
-    printf((char *)ARTUNDERMSG);
-    return errno;
+    printf(ARTUNDERMSG);
     break;
   case userDivideByZero:
-    errno = userDivideByZero;
-    setErrMode();
-    printf((char *)DOMMSG);
-    return errno;
+    printf(DOMMSG);
     break;
   default:
-    errno = SUCCESS;
-    setErrMode();
-    printf((char*) userInvalidInput);
-    return errno;
+    printf("INVALID INPUT");
   }
 }
 
@@ -67,51 +49,51 @@ int arithmeticError(int left, int right, char operation) {
   switch (operation) {
     case '+':
       if (right > 0 && left > INT_MAX - right) { // b = intMax & a = intMax
-        return errno = userArithmeticOverflow;
+        return userArithmeticOverflow;
       }
       if (right < 0 && left < INT_MIN - right) { // b = intMin & a = intMin
-        return errno = userArithemticUnderflow;
+        return userArithemticUnderflow;
       }
     case '-':
       if (right < 0 && left > INT_MAX + right) {
-        return errno = userArithmeticOverflow; // a - (–|b|)  ?  +Overflow
+        return userArithmeticOverflow; // a - (–|b|)  ?  +Overflow
       }
       if (right > 0 && left < INT_MIN + right) {
-        return errno = userArithemticUnderflow; // a - (+|b|)  ?  -Underflow
+        return userArithemticUnderflow; // a - (+|b|)  ?  -Underflow
       }
     case '*':
       if (left == 0 || right == 0) { // a or b is 0
-        return errno = SUCCESS;
+        return SUCCESS;
       }
       if (right > 0) {                // b > 0
         if (left > INT_MAX / right) { // a = intMax b = intMax Overflow (+)
           // unused memory range ( that we want to use) is smaller than the other
           // number
-          return errno = userArithmeticOverflow;
+          return  userArithmeticOverflow;
         }
         if (left < INT_MIN / right) { // a = intMin b = intMax  Underflow (-)
           // unused memory range (that we want to use) is not enough in the other
           // way
-          return errno = userArithemticUnderflow;
+          return  userArithemticUnderflow;
         }
       }
       // right < 0
       if (left > 0 && right < INT_MIN / left) { // a = intMax & b = intMin overflow
-        return errno = userArithemticUnderflow;
+        return  userArithemticUnderflow;
       }
       if (left < 0 && right < 0 && right < INT_MAX / left) { // a = intMin (smallest negative) & b =
                                     // intMin(smallest negative) & b is even
                                     // smaller than memoryrange underflow
-        return errno = userArithmeticOverflow;
+        return  userArithmeticOverflow;
       }
     case '/':
       if (left == 0) { // denominator is 0
-        return errno = userDivideByZero;
+        return userDivideByZero;
       }
       if (left == INT_MIN && right == -1) { // a = minInt / -1 overflow
-        return errno = userArithmeticOverflow;
+        return  userArithmeticOverflow;
       }
     default:
-      return errno = SUCCESS;
+      return  SUCCESS;
     }
 }
